5dc8dcdbe9c05f9f20d2d2485dac1fcd0a9123dc
seidh <nicolas.dasseler@gmail.com>
2014-10-16 14:52:27 +0200

    Ajout de proxy reverse

--- application/config/config.php
+++ application/config/config.php
@@ -355,7 +355,7 @@ $config['rewrite_short_tags'] = FALSE;
 | Comma-delimited, e.g. '10.0.1.200,10.0.1.201'
 |
 */
-$config['proxy_ips'] = '';
+$config['proxy_ips'] = '192.168.0.16';
 
 
 /* End of file config.php */
