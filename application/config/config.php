4bf1d6358463cbdf29750c30c0501e4d45617da9
seidh <nicolas.dasseler@gmail.com>
2014-10-23 14:23:34 +0200

    change url

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url'] = 'https://open-note.ddns.net/';
+$config['base_url'] = '';
 
 /*
 |--------------------------------------------------------------------------
