8cb5076e6ccfb743a80bc0f53b8c5f281512578b
Jonathan <Jonathan@JiksaaWorks>
2014-10-24 16:21:39 +0200

    Adding administration model file with functions definition

---
+++ application/models/administration_model.php
@@ -0,0 +1,52 @@
+<?php
+Class Admin extends CI_Model {
+
+    /**
+     * Model functions about users
+     */
+    function get_all_user(){
+        
+    }
+    
+    function modify_users($fields,$id){
+        
+    }
+    
+    function create_users($fields){
+        
+    }
+
+    /**
+     * Model functions about category
+     */
+    function get_all_category(){
+        
+    }
+    
+    function modify_category($fields,$id){
+        
+    }
+    
+    function create_category($fields){
+        
+    }
+    
+    function attribute_moderator($id_user,$id_cat){
+        
+    }
+
+    /**
+     * Model functions about moderator
+     */
+    function get_all_modérateur(){
+        
+    }
+
+    /**
+     * Model function about moderator
+     */
+    function get_all_notes(){
+        
+    }
+
+}
