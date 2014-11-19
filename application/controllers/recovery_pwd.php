e441e7069690980999c57d9ef1a1651a5dbb6ff7
seidh <nicolas.dasseler@gmail.com>
2014-11-19 16:17:48 +0100

    ADD check mail

--- application/controllers/recovery_pwd.php
+++ application/controllers/recovery_pwd.php
@@ -15,18 +15,49 @@ class recovery_pwd extends CI_Controller {
  }
  function sendMail()
  {
-    $this->load->library('email');
+    $this->load->library('form_validation');
+    $this->form_validation->set_rules('email', 'Adresse mail', 'trim|required|xss_clean|callback_check_email');
+    
+    if ($this->form_validation->run() == FALSE)
+    {
+        //email inexistant
+        $this->load->view('recovery_pwd_view');
+    }
+    else
+    {
+        $this->load->library('email');
 
-    $this->email->from('recovery@open-note.ddns.net', 'Admin');
-    $this->email->to($this->input->post('email'));
+        $this->email->from('recovery@open-note.ddns.net', 'Admin');
+        $this->email->to($this->input->post('email'));
 
-    $this->email->subject('Récupération de votre mot de passe');
-    $this->email->message('Testing the email class');	
+        $this->email->subject('Récupération de votre mot de passe');
+        $this->email->message('Testing the email class');	
 
-    $this->email->send();
+        $this->email->send();
 
-    echo $this->email->print_debugger();
+        echo $this->email->print_debugger();
+    }
+    
  }
+ 
+    function check_email($email)
+    {
+        $this -> db -> select('id, email, pwd');
+        $this -> db -> from('user');
+        $this -> db -> where('email', $email);
+        $this -> db -> limit(1);
+ 
+        $query = $this -> db -> get();
+        if($query -> num_rows() == 1)
+        {
+                return true;
+        }
+        else
+        {
+            $this->form_validation->set_message('check_email', 'Adresse mail inexistante');
+            return false;
+        }
+    }
  
 }
  
