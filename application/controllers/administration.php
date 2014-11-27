8416e1caf5932e8b4b3dce6c6e87fe68e1b29752
Onza1 <vroman.olivier@gmail.com>
2014-11-27 10:59:42 +0100

    - edit user

--- application/controllers/administration.php
+++ application/controllers/administration.php
@@ -91,51 +91,97 @@ class administration extends CI_Controller
         
         function adduser()
         {
-            $this->form_validation->set_rules('name', 'Nom', 'trim|required|xss_clean');
-            $this->form_validation->set_rules('firstname', 'Prénom', 'trim|required|xss_clean');
-            $this->form_validation->set_rules('sexe','Sexe', 'trim|required|xss_clean');
-            $this->form_validation->set_rules('email', 'Email', 'trim|required|xss_clean');
-            $this->form_validation->set_rules('groupe', 'Groupe', 'trim|required|xss_clean');
-            $this->form_validation->set_rules('dayBirth', 'Jour de naissance', 'trim|required|xss_clean');
-            $this->form_validation->set_rules('monthBirth', 'Mois de naissance', 'trim|required|xss_clean');
-            $this->form_validation->set_rules('yearBirth', 'Année de naissance', 'trim|required|xss_clean');
+            $this->form_validation->set_rules('newName', 'Nom', 'trim|required|xss_clean');
+            $this->form_validation->set_rules('newFirstname', 'Prénom', 'trim|required|xss_clean');
+            //$this->form_validation->set_rules('sexe','Sexe', 'trim|required|xss_clean');
+            $this->form_validation->set_rules('newEmail', 'Email', 'trim|required|xss_clean|callback_check_email');
+            $this->form_validation->set_rules('newGroup', 'Groupe', 'trim|required|xss_clean');
+            $this->form_validation->set_rules('newPseudo', 'Pseudo', 'trim|required|xss_clean');
+            //$this->form_validation->set_rules('dayBirth', 'Jour de naissance', 'trim|required|xss_clean');
+            //$this->form_validation->set_rules('monthBirth', 'Mois de naissance', 'trim|required|xss_clean');
+            //$this->form_validation->set_rules('yearBirth', 'Année de naissance', 'trim|required|xss_clean');
             
             if($this->form_validation->run() == FALSE)
             {
                 //erreur dans le formulaire
-                redirect('newUserForm', 'refresh');
+
+                redirect('administration/userList', 'refresh');
+                
             }
             else
             {
                 //build default user password by concat name and firstname (tolowercase)
-                $user_default_pwd = strtolower($this->input->post('name') . $this->input->post('firstname'));
+                $user_default_pwd = strtolower($this->input->post('newName') . $this->input->post('newFirstname'));
                 
                 //build user data array to inject into database
-                $user_data = array('name' => $this->input->post('name'),
-                        'firstname' => $this->input->post('firstname'),
+                $user_data = array('name' => $this->input->post('newName'),
+                        'firstname' => $this->input->post('newFirstname'),
                         'pwd' => sha1($user_default_pwd),
-                        'email' => $this->input->post('email'),
-                        'birthday' => $this->input->post('yearBirth').'-'.$this->input->post('monthBirth').'-'.$this->input->post('dayBirth'),
-                        'groupe' => $this->input->post('groupe'),
-                        'sexe' => $this->input->post('sexe'),
-                        'pseudo' => $this->input->post('name').$this->input->post('firstname'),
+                        'email' => $this->input->post('newEmail'),
+                        //'birthday' => $this->input->post('yearBirth').'-'.$this->input->post('monthBirth').'-'.$this->input->post('dayBirth'),
+                        'birthday' => null,
+                        'groupe' => $this->input->post('newGroup'),
+                        //'sexe' => $this->input->post('sexe'),
+                        'sexe' => null,
+                        'pseudo' => $this->input->post('newPseudo'),
                         'avatar' => null);
                 
-                print_r($user_data);//debug instruction
+                //print_r($user_data);//debug instruction
                 
                 //check if unique fields aren't used yet
-                if(!$this->administration_model->is_user_exist($user_data['email']))
+                if($this->administration_model->is_user_exist($user_data['email']))
                 {
                     //user already exist so put error message
                 }
                 //else add user inside database
                 $this->administration_model->create_user($user_data);
+                redirect("administration/userList","refresh");
             }
         }
         
         function modify_user($id){
             
+            $this->form_validation->set_rules('editName', 'Nom', 'trim|required|xss_clean');
+            $this->form_validation->set_rules('editFirstname', 'Prénom', 'trim|required|xss_clean');
+            $this->form_validation->set_rules('editEmail', 'Email', 'trim|required|xss_clean|callback_check_email');
+            $this->form_validation->set_rules('editGroup', 'Groupe', 'trim|required|xss_clean');
+            $this->form_validation->set_rules('editPseudo', 'Pseudo', 'trim|required|xss_clean');
             
+            if($this->form_validation->run() == FALSE)
+            {
+                //erreur dans le formulaire
+                echo'Debug';
+                //redirect('administration/userList', 'refresh');
+                
+            }
+            else
+            {
+//                echo $this->input->post('editFirstname'); 
+//                echo $this->input->post('editName'); 
+//                echo $this->input->post('editPseudo'); 
+//                echo $this->input->post('editEmail'); 
+//                echo $this->input->post('editGroup'); 
+                
+                //build user data array to inject into database
+                $user_data = array('name' => $this->input->post('editName'),
+                        'firstname' => $this->input->post('editFirstname'),                        
+                        'email' => $this->input->post('editEmail'),
+                        'groupe' => $this->input->post('editGroup'),
+                        'pseudo' => $this->input->post('editPseudo'));
+                
+                //print_r($user_data);//debug instruction
+                
+                //check if unique fields aren't used yet
+                if($this->administration_model->is_user_exist($user_data['email']))
+                {
+                    //user already exist so put error message
+                    echo'Error';
+                }
+                //else add user inside database
+                $this->administration_model->modify_user($user_data, "id = $id");
+                redirect("administration/userList","refresh");
+            }
+
         }
 	/*function logout()
         {
@@ -151,15 +197,58 @@ class administration extends CI_Controller
             $this->data['keywords'] = 'les, mots, clés, de, la, page';
 
             // on choisit la view qui contient le corps de la page
-            $this->data['contents'] = 'admin_modoList_view'; //TODO : sync with Olivier view
+            $this->data['contents'] = 'admin_moderator_list'; //TODO : sync with Olivier view
             
-            $this->data['moderators_data'] = $this->administation_model->get_all_moderators();
+            $this->data['users_data'] = $this->administration_model->get_all_user();
+            $this->data['moderators_data'] = $this->administration_model->get_all_moderator();
+            $this->data['category_data'] = $this->administration_model->get_all_category();
             
             // on charge la page dans le template
             $this->load->view('templates/template', $this->data); 
         }
         
         function addModoForm()
+        {
+            
+            $this->form_validation->set_rules('user', 'Utilisateur', 'trim|required|xss_clean');
+            $this->form_validation->set_rules('confirm', 'Pseudo', 'trim|required|xss_clean');
+            
+            if($this->form_validation->run() == FALSE)
+            {
+                //erreur dans le formulaire
+                echo'DEBUG';
+                //redirect('administration/userList', 'refresh');
+                
+            }
+            else
+            {
+//                echo $this->input->post('editFirstname'); 
+//                echo $this->input->post('editName'); 
+//                echo $this->input->post('editPseudo'); 
+//                echo $this->input->post('editEmail'); 
+//                echo $this->input->post('editGroup'); 
+                
+                //build user data array to inject into database
+                $assignation = array('name' => $this->input->post('user'),
+                        'firstname' => $this->input->post('editFirstname'),                        
+                        'email' => $this->input->post('editEmail'),
+                        'groupe' => $this->input->post('editGroup'),
+                        'pseudo' => $this->input->post('editPseudo'));
+                
+                //print_r($user_data);//debug instruction
+                
+                //check if unique fields aren't used yet
+                if($this->administration_model->is_user_exist($user_data['email']))
+                {
+                    //user already exist so put error message
+                }
+                //else add user inside database
+                $this->administration_model->modify_user($user_data, "id = $id");
+                redirect("administration/userList","refresh");
+            }
+        }
+        
+        function assignModoForm()
         {
             //TODO controller form side
             // définition des données variables du template
@@ -173,7 +262,6 @@ class administration extends CI_Controller
             // on charge la page dans le template
             $this->load->view('templates/template', $this->data); 
         }
-
         
         function notesList()
         {
@@ -207,8 +295,9 @@ class administration extends CI_Controller
             $this->data['keywords'] = 'les, mots, clés, de, la, page';
             
             // on choisit la view qui contient le corps de la page
-            $this->data['contents'] = 'admin_categoriesList_view'; //TODO : sync with Olivier view
-
+            $this->data['contents'] = 'admin_cat_list'; //TODO : sync with Olivier view
+            
+            $this->data['cat_moderators'] = $this->administration_model->get_all_moderator();
             $this->data['cat_datas'] = $this->administration_model->get_all_category();
             
             // on charge la page dans le template
