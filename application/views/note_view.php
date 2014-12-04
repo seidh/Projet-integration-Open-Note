2738c0caf70cb3a11f8b45ca72ac2e2f4eb3c6cb
seidh <nicolas.dasseler@gmail.com>
2014-12-04 10:40:56 +0100

    ADD diff view

--- application/views/note_view.php
+++ application/views/note_view.php
@@ -67,7 +67,10 @@
                                     echo form_close();
                                     echo'</div>';
                                     echo'<div class="col-sm-4">';
-                                    echo '<button type="button" class="btn btn-outline btn-primary btn-sm">Voir la différence entre cette version et le version actuel</button>';
+                                    echo form_open('note/view_diff/' . $commit['commit_hash']);
+                                    echo '<button type="submit" class="btn btn-outline btn-primary btn-sm">Voir la différence entre cette version et le version actuel</button>';
+                                    echo '<input class="form-control"  name="note_id" id="note_id" type="text" style="display: none" value="' . $note['id'] . '" />';
+                                    echo form_close();
                                     echo'</div>';
                                     echo'</div>';
                                     echo'</div>';
